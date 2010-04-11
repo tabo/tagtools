c20b936cd91e572da95ddc1309befcdcfde835c9
Gustavo Picon <tabo@tabo.pe>
2010-04-11 17:32:06 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+version = '0.8a'
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.4",
+    "Programming Language :: Python :: 2.5",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.0",
+    "Programming Language :: Python :: 3.1",
+    "Operating System :: OS Independent",
+    "Topic :: Software Development :: Libraries",
+]
+
+root_dir = os.path.dirname(__file__)
+if not root_dir:
+    root_dir = '.'
+long_desc = open(root_dir + '/README').read()
+
+setup(
+    name='tagtools',
+    version=version,
+    url='http://code.tabo.pe/tagtools/',
+    author='Gustavo Picon',
+    author_email='tabo@tabo.pe',
+    license='Apache License 2.0',
+    py_modules=['tagtools'],
+    description='Python helpers to work with tags.',
+    classifiers=classifiers,
+    long_description=long_desc,
+)
